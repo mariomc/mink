46f96eb23f15d7bbc0d45ac2b74cbd898b34a25d
Mário Gonçalves <mario.mc.goncalves@gmail.com>
2014-04-21 17:20:55 +0100

    Gruntfile requirejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,22 @@ module.exports = function (grunt) {
 	      output: "ender",
 	      dependencies: ["bean", "bonzo@v1.4.0", "qwery"]
 	    }
+	  },
+	  requirejs: {
+	    compile: {
+	      options: {
+	        baseUrl: ".",
+	        name: "dist/modal/modal.js", // assumes a production build using almond
+	        out: "all/mink-all.js",
+	        preserveLicenseComments: false,
+	        generateSourceMaps: true,
+	        optimize: "uglify2"
+	      }
+	    }
 	  }
 	});
 	grunt.loadNpmTasks('grunt-ender');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
 
 	grunt.registerTask('default', ['ender']);
 };
