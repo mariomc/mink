40c1ba7a7cdada81f7ce5d4333598984b33879ae
Mário Gonçalves <mario.mc.goncalves@gmail.com>
2014-05-13 00:43:19 +0100

    Re-adding contains method

--- ender/ender.js
+++ ender/ender.js
@@ -129,6 +129,16 @@
 	  return store
 	}
 
+	function contains(container, element){
+		do {
+			element = element.parentNode;
+			if (element === container) {
+				return true;
+				}
+			} while (element);
+			return false;
+	}
+
 
 	isArray = Array.isArray || function(object){ return object instanceof Array };
 
@@ -147,7 +157,8 @@
 		isPlainObject: isPlainObject,
 		extend: extend,
 		proxy: proxy,
-		camelCase: camelize
+		camelCase: camelize,
+		contains: contains
 	})
 
 	$.fn.data = function(name, value) {
