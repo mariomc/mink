4c585979f67c1e092f9e60d3753cf6285f3fb416
Mário Gonçalves <mario.mc.goncalves@gmail.com>
2014-06-02 15:31:12 +0100

    feat: add commonJS support to UMD definition

--- base/base.js
+++ base/base.js
@@ -1,31 +1,34 @@
 (function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous this.
-        define(factory);
-    } else {
-        // Browser globals
-        factory();
-    }
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous this.
+    define(factory);
+  } else if (typeof exports === 'object') {
+    // Node/CommonJS
+    factory();
+  } else {
+    // Browser globals
+    factory();
+  }
 }(function () {
-	// Get the helper API from the current script element. Just so you can redefine a custom helper without having to open a script tag before mink.
+  // Get the helper API from the current script element. Just so you can redefine a custom helper without having to open a script tag before mink.
 
-	var currentScript = document.currentScript;
-	if(!currentScript){
-		// TODO: This approach has a shortcoming with AMD loading.
-		var scripts = document.getElementsByTagName('script');
-		currentScript = scripts[scripts.length - 1];
-	}
+  var currentScript = document.currentScript;
+  if(!currentScript){
+    // TODO: This approach has a shortcoming with AMD loading.
+    var scripts = document.getElementsByTagName('script');
+    currentScript = scripts[scripts.length - 1];
+  }
 
-	// If mink is previously defined, use that definition.
+  // If mink is previously defined, use that definition.
 
-	window.mink = window.mink || {};
+  window.mink = window.mink || {};
 
-	// Helper hierarchy. 
-	// Programmatical definition comes first. Definition in data-attribute comes second. Zepto comes third for mobile first as a default, if present. Ender comes after as our packaged helper. jQuery after that. kInk and $ if all else fails.
+  // Helper hierarchy. 
+  // Programmatical definition comes first. Definition in data-attribute comes second. Zepto comes third for mobile first as a default, if present. Ender comes after as our packaged helper. jQuery after that. kInk and $ if all else fails.
 
-	window.mink.helper = window.mink.helper || window[currentScript.getAttribute('data-helper')] || window.Zepto || window.ender || window.jQuery || window.kink || window.$ || {};
+  window.mink.helper = window.mink.helper || window[currentScript.getAttribute('data-helper')] || window.Zepto || window.ender || window.jQuery || window.kink || window.$ || {};
 
-	window.mink.$ = window.mink.helper;
+  window.mink.$ = window.mink.helper;
 
-	return window.mink.helper;
+  return window.mink.helper;
 }));
